674e57b7e30695afb8f3cd8429844d05d0c67cc7
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-17 15:57:57 +0200

    click on Bart add

--- js/first.js
+++ js/first.js
@@ -1,3 +1,10 @@
 // Recupero gli elementi dal DOM
 const listElement = document.getElementById('text-container');
 const bart = document.getElementById('bart');
+
+// In ascolto su Bart
+bart.addEventListener('click', function(){
+    // Chiedo all'utente la frase da scrivere
+	const phrase = prompt('Cosa vuoi che scriva?').trim();
+    console.log(phrase);
+});
