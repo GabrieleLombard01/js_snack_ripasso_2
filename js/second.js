00395d2b19817a0dc54144a1d55b10a4767f0679
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-17 16:23:07 +0200

    components add

--- js/second.js
+++ js/second.js
@@ -1,8 +1,16 @@
+/*
+Cliccando su bart, ci chiede quanti clienti ha la classe. Ci chiede i nomi e poi li scrive alla lavagna
+*/
+
 // Recupero gli elementi dal DOM
 const listElement = document.getElementById('text-container');
 const bart = document.getElementById('bart');
 
 // In ascolto su Bart
 bart.addEventListener('click', function(){
-    
+
+    // Chiedo all'utente i componenti della classe
+	const components = parseInt(prompt('Da quanti componenti è formata la classe?'));
+    console.log(components);
+
 });
