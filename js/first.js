7d0ef9853180d43a7d9288660cad7b33cfd40b09
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-17 15:55:27 +0200

    first commit

---
+++ js/first.js
@@ -0,0 +1,3 @@
+// Recupero gli elementi dal DOM
+const listElement = document.getElementById('text-container');
+const bart = document.getElementById('bart');
---
+++ steps.md
@@ -0,0 +1,5 @@
+# TRACCIA 1:
+
+Cliccando su bart, ci chiede cosa scrivere e quante volte. Date le risposte, lui scrive la stessa frase tot volte.
+
+<hr>
