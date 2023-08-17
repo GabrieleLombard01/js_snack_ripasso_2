68b66429932c7527168a871ff37d15e4bcf1ee1d
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-17 16:20:45 +0200

    Dom elements

---
+++ js/second.js
@@ -0,0 +1,8 @@
+// Recupero gli elementi dal DOM
+const listElement = document.getElementById('text-container');
+const bart = document.getElementById('bart');
+
+// In ascolto su Bart
+bart.addEventListener('click', function(){
+    
+});
