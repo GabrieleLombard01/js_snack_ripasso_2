60d7361628722468f6bc99911409d2499c61cc3b
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-17 16:38:51 +0200

    third track add

---
+++ js/third.js
@@ -0,0 +1,11 @@
+/*
+Cliccando su bart, ci chiede quanti clienti ha la classe. Ci chiede i nomi e poi li scrive alla lavagna
+*/
+
+// Recupero gli elementi dal DOM
+const listElement = document.getElementById('text-container');
+const bart = document.getElementById('bart');
+
+// In ascolto su Bart
+bart.addEventListener('click', function(){
+});
