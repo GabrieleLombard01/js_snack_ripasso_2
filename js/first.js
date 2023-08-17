42929edefdf2ca509ce56dbfa6901f7d7d02ba2a
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-17 15:59:16 +0200

    times add

--- js/first.js
+++ js/first.js
@@ -4,7 +4,13 @@ const bart = document.getElementById('bart');
 
 // In ascolto su Bart
 bart.addEventListener('click', function(){
+
     // Chiedo all'utente la frase da scrivere
 	const phrase = prompt('Cosa vuoi che scriva?').trim();
     console.log(phrase);
+
+    // Chiedo all'utente quante volte scrivere
+	const times = parseInt(promt('Quante volte? '));
+    console.log(times);
+
 });
